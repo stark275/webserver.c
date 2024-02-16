7570a48262fb4b60313d60f0bc04f8a5bf414152
stark <stark1_5@live.com>
2024-02-16 23:53:35 +0100

    Dynamic memory allocation

--- client/client.c
+++ client/client.c
@@ -3,8 +3,10 @@
 
 int main() {
     // Exécute la commande wget trois fois
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 2; ++i) {
         system("wget localhost:8006/Index.html");
+        system("wget localhost:8006/login.html");
+
     }
 
     printf("Téléchargement terminé !\n");
